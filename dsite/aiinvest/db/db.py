31578cb6aa2664cf8376b98864dfed1f3fcec18e
happy <jiangzhanling@gmail.com>
2023-12-18 23:58:46 +0000

    update:db function udpate

--- dsite/aiinvest/db/db.py
+++ dsite/aiinvest/db/db.py
@@ -20,6 +20,9 @@ DEFAULT_PASSWORD = os.environ.get("DB_PASS")
 print("DB_PASS is  ", DEFAULT_PASSWORD)
 DEFAULT_PORT = dbs['PORT']
 
+######### to be added: encryption function (for password related content)
+
+
 # get a connection to database. inner function. 
 # Open a cursor to perform database operations
 def _get_conn(db=DEFAULT_DATABASE, dbhost=DEFAULT_HOST, dbport=DEFAULT_PORT, dbuser=DEFAULT_USER,dbpass=DEFAULT_PASSWORD):
@@ -32,13 +35,26 @@ def _get_conn(db=DEFAULT_DATABASE, dbhost=DEFAULT_HOST, dbport=DEFAULT_PORT, dbu
         except Exception as ex:
             format_excetpion_message(ex)
 
+# public function: to get a specific asset's history price data
+# firstly get it from the database. if the database does not have the asset's data or all of it's data.
+# then build it's data in the database.
+def get_asset_price_data(asset_name=""):
+     
+     pass
 
 
+# private function: to get data for an condition in a table.
+# condition is a dictionary. example: {'asset_name': TSLA, 'Date': '2023-12-18 00:00:00'}
+def _get_data_from_db(condition_dict={}, table_name=""):
+     
+     pass
+
+
+# public function
 # get the asset id by asset name. 
 # if can't find the id. check it online, 
 # if the asset is an valid ticker, insert it into the table
 # otherwise raise valueerror.
-# 
 def get_asset_id(asset_name=""):
         asset_id = -1
         try:
@@ -46,23 +62,32 @@ def get_asset_id(asset_name=""):
             query = "SELECT id from aiinvest_assetlist where asset_name=\'" + asset_name +"\'"
             conn = _get_conn()
             cur = conn.cursor()
-            cur.execute(query)
-            rows = cur.fetchall()
-            conn.commit()
-            cur.close()
 
-            if DEBUG:
-                print(rows)
+            is_finish = False
+            while not is_finish:
+                 
+                cur.execute(query)
+                rows = cur.fetchall()
+
+                if DEBUG:
+                    print(rows)
+                
+                # get asset_id in the database
+                if len(rows) == 1:
+                    asset_id = rows[0][0]
+                    # break the loop
+                    is_finish = True
+                # if the asset doesn't exist in the database. insert it into it
+                elif len(rows) == 0 and Asset(asset_name).is_valid():
+                    _add_asset_to_list(asset_name, ASSETLIST_TABLE_NAME)
+                    # then continue the loop to get the ID
+                else:
+                    cur.close()
+                    conn.close()
+                    raise ValueError(f"there is {len(rows)} asset in the db for asset_name: {asset_name}. or the asset_name may be an invalid ticker")
             
-            if len(rows) == 1:
-                asset_id = rows[0][0]
-            # if the asset doesn't exist in the database. insert it into it
-            elif len(rows) == 0 and Asset(asset_name).is_valid():
-                 asset_id = _add_asset_to_list(asset_name, ASSETLIST_TABLE_NAME)
-            else:
-                  conn.close()
-                  raise ValueError(f"there is {len(rows)} asset in the db for asset_name: {asset_name}. or the asset_name may be an invalid ticker")
-            
+            conn.commit()
+            cur.close()           
             conn.close()
             return asset_id
         except Exception as ex:
@@ -82,7 +107,7 @@ def _add_asset_to_list(asset_name="", table_name=""):
                     print(f" asset_name is {asset_name} and table name is {table_name} in _add_asset_to_list",  )
                 
                 _add_data_to_db(asset_name, table_name)
- 
+
             # type of the args data inputs are not correct, 
             else:
                 raise TypeError(
@@ -92,9 +117,7 @@ def _add_asset_to_list(asset_name="", table_name=""):
             format_excetpion_message(ex)
 
 
-
-# get_asset_id("TSLA")
-
+######### to be improved : years to more flexialbe xD, xM, xY, Max
 # get data online and save to database. 
 # years is number of yesrs to build the data. default is the current year till now
 def _build_asset_data(asset_name="", years=0, table_name=ASSETDATA_TABLE_NAME):
@@ -104,13 +127,14 @@ def _build_asset_data(asset_name="", years=0, table_name=ASSETDATA_TABLE_NAME):
             if not asset.is_valid():
                 raise ValueError (f"Asset data is not found for Asset name : {asset_name}")
             
-            # asset_id = get_asset_id(asset_name)
-            asset_id = 1
+            asset_id = get_asset_id(asset_name)
 
             his_price = asset.fetch_his_price(period=years)
 
-            his_price['name'] = asset_name
+            his_price['asset_name'] = asset_name
+            
             his_price['asset_id'] = asset_id
+
             his_price.pop('Adj Close')
             # his_price = his_price.rename(columns={'Date': 'asset_data_date', 
             #                           'Open': 'asset_open_price', 
@@ -119,26 +143,20 @@ def _build_asset_data(asset_name="", years=0, table_name=ASSETDATA_TABLE_NAME):
             #                           'Close': 'asset_close_price',
             #                           'Volume': 'asset_volume'
             #                           })
-            his_price = his_price.set_index(['name'],append=True)
+            his_price = his_price.set_index(['asset_name'],append=True)
             his_price.columns = map(str.lower, his_price.columns)
-            print(his_price.index)
-            print(his_price)
-            
-
-            # _add_data_to_db()
-                 # add the price to the table.
-            # print("his_price.index ------ ",his_price.index)
-            # print("his_price.columns ------ ",his_price.columns)
-
-            for price in his_price:
-                # print("his_price.loc[0] ------ ",his_price.loc[0])
-                print("price ------ ",price)
-                # 
-            # _add_data_to_db(his_price,table_name)
-
 
             if DEBUG:
                 print(len(his_price))
+                print("asset_id is ", asset_id)
+                # print(his_price)
+            
+            # add the history price information for the asset to the database.
+            _add_data_to_db(his_price,table_name)
+
+            # this will print all the name for the columns
+            # for price in his_price:
+            #     print("price ------ ",price)
 
         except Exception as ex:
             format_excetpion_message(ex)
@@ -164,63 +182,79 @@ def _add_data_to_db(data_list=pd.DataFrame(), table_name=""):
 
                 # provide information for debug
                 if DEBUG:
-                    print("data_list is\n", data_list)
-                    print("£££££££££££££££ ",data_list.columns)
-                    print("data_list.index.names--------\n", data_list.index.names)
+                    # print("data_list is\n", data_list)
+                    print("_add_data_to_db: data_list.columns are ",data_list.columns)
+                    print("_add_data_to_db: data_list.index.names are ", data_list.index.names)
 
+                data_list = data_list.convert_dtypes()
+                # print("data_list.values[1]", data_list.values[1])
                 #  construct the query for add rows to the table
                 query = "INSERT INTO "+ table_name + " ("
                 
-                start_column = 0
-                if data_list.index.names[0] == None:
-                    start_column = 1
+                is_index_none = False
+                if data_list.index.names[0] is None:
+                    is_index_none = True
 
-                for _ in data_list.columns[start_column:]:
-                     query = query + f"{_} ,"
+                # all the columns names include indexes.
+                all_columns = list(filter(None, data_list.index.names + data_list.columns.to_list()))
+                print("all_columns", all_columns)
+
+                for _ in all_columns:
+                        query = query + f"{_} ,"
 
                 query = query.rstrip(query[-1])+") SELECT "
 
+                # each row of the Data_list is a row to be inserted.
                 i = 0
                 while i < len(data_list.index):
-                    # each row of the Data_list is a row to be inserted.
+
                     queryi = query
-                    for _ in data_list.columns[start_column:]:
-                        print(" $$$$$$$$$$$ _ is \n  ", _)
-                        queryi = queryi + f"'{data_list.iloc[i][_]}'," 
+
+                    # if index.name == None. the index has no name and was added by dataframe. 
+                    # it will not be added to the database.
+                    # otherwise, add index values to the query. 
+                    if not is_index_none:
+                        for _ in data_list.index[i]:
+                            # print(f"index {i} values are ", data_list.index[i])
+                            queryi = queryi + f"'{_}',"
                     
+                    # add non-index values to the query. 
+                    k = 0
+                    while k < len(data_list.columns):
+                        # print(f"{_} is  ", data_list.iloc[i][_])
+                        queryi = queryi + f"'{data_list.values[i][k]}'," 
+                        k += 1
+                    # print("data_list.iloc[i] is ",data_list.iloc[i])
+                        
+                    # avoid insert repeatidly
+                    # construct the unique identifiers of the data_list and avoid duplicate data to be added
+                    # if index is not assigned to the unique identifier. set the second column (column[1]) as the identifier
+                    # any data has the same identifier in the database will not be added.
+                    # remove the last comma and add conditions : the data does not exist in the database.
                     queryi = queryi.rstrip(queryi[-1]) + " WHERE NOT EXISTS (SELECT * FROM " + table_name + " WHERE "
                     
-                    # if index is not assigned to the unique identifier. set the second column (column[1]) as the identifier
+                    # if index is not assigned to the unique identifier. set the first column (column[0]) as the identifier
                     if data_list.index.names[0] == None:
-                        queryi = queryi + data_list.columns[1] + f" = '{data_list.iloc[i][data_list.columns[1]]}' and "
+                        queryi = queryi + data_list.columns[0] + f" = '{data_list.iloc[i][data_list.columns[0]]}' and "
                     # if the index is provided . use all the indexes as the unique identifier.
                     else:
-                         for _ in data_list.index.names:
-                            # queryi = queryi + _ + f" = '{data_list.iloc[i][_]}' and"
-                            print("********** _ is ", _)
-                            # print(data_list.iloc[i])
-                            print("data_list.index[i] is ", data_list.index.get_level_values(_))
-                            # print("data_list.index.value is ", data_list.index.get_value(_))
-                    
+                            j = 0
+                            for _ in data_list.index.names:
+                                # data_list.index[i] is the row i indexes. 
+                                # data_list.index[i][j] is the jth index value in row i
+                                # print(f"data_list.index[{i}][{j}] name is {_} and the value is ", data_list.index[i][j])
+                                queryi = queryi + _ + " = '" + str(data_list.index[i][j]) + "' and "
+                                j =+ 1
+                        
                     queryi = queryi.rstrip().rstrip("and") + ");"
                     i += 1
-                    print(queryi)
-                
-                # print("column 0 ---", data_list.columns[0])
-                # print("Index 0 ---", data_list.index[0])
-                    ############################# to be continue : avoid insert repeatidly
-                
-                # construct the unique identifiers of the data_list and avoid duplicate data to be added
-                # if index is not assigned to the unique identifier. set the second column (column[1]) as the identifier
-                # any data has the same identifier in the database will not be added.
-                
-                
-                for _ in data_list.index.names:
-                        print(f"Asset index {_} are:" , _)
+                    # print(queryi)
+                    cur.execute(queryi)
 
 
                 conn.commit()
                 conn.close
+
             # input list is empty
             else:
                 raise Exception("The input data list is empty for _add_data_to_db or tablename")
@@ -253,7 +287,8 @@ def is_table_valid(conn, table_name=""):
              format_excetpion_message(ex)
         return exist
 
-_build_asset_data("TSLA", 0)
+
+_build_asset_data("AAPL", 0)
 
 
 # ##################
@@ -267,7 +302,7 @@ def test_tmp():
         # asset_name = asset_name.set_index(['asset_name', 'date'])
         print(data_list.index)
         if data_list.index.names[0] == None:
-             print("@@@@@@@@@@@@@@")
+             print("datalist with no index")
         
         print("@@@@@@@@@@@@@@data.columns", data_list.columns)
 
@@ -289,14 +324,23 @@ def test_tmp():
 
         query = query.rstrip(query[-1])+") SELECT "
 
+        # each row of the Data_list is a row to be inserted.
         i = 0
         while i < len(data_list.index):
-            # each row of the Data_list is a row to be inserted.
+
             queryi = query
+            # add index values to the query. 
+            for _ in data_list.index[i]:
+                print("index i values are ", data_list.index[i])
+                queryi = queryi + f"'{_}',"
+            
+            # add non-index values to the query. 
             for _ in data_list.columns[start_column:]:
                 print(" $$$$$$$$$$$ _ is \n  ", _)
+                
                 queryi = queryi + f"'{data_list.iloc[i][_]}'," 
             
+            # remove the last comma and add conditions : the data does not exist in the database.
             queryi = queryi.rstrip(queryi[-1]) + " WHERE NOT EXISTS (SELECT * FROM " + table_name + " WHERE "
             
             # if index is not assigned to the unique identifier. set the second column (column[1]) as the identifier
@@ -306,9 +350,7 @@ def test_tmp():
             else:
                     j = 0
                     for _ in data_list.index.names:
-                        # queryi = queryi + _ + f" = '{data_list.iloc[i][_]}' and"
                         print("********** _ is ", _)
-                        # print(data_list.iloc[i])
                         print("data_list.index[i][j] is ", data_list.index[i][j])
                         queryi = queryi + _ + " = '" + data_list.index[i][j] + "' and "
                         j =+ 1
@@ -372,4 +414,4 @@ cur.close()
 conn.close()
 '''
 
-test_tmp()
+# test_tmp()
